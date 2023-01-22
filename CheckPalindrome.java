b38c6c5f1fd8558be5933f4384ca5d02f7a6d9e3
amantyagi23 <119286791+amantyagi23@users.noreply.github.com>
2023-01-22 15:12:01 +0530

    Update CheckPalindrome.java

--- CheckPalindrome.java
+++ CheckPalindrome.java
@@ -1,24 +1,18 @@
 public class CheckPalindrome {
     static int num = 0;
-    static int count(int n){
-        if(n==0){
-            return 0;
-        }
-        return count(n/10)+1;
-    }
-    static int checkNo(int n,int count){
+    static int checkNo(int n){
         if(n==0){
             return 0;
         }
         int a = n%10;
-        num =num+ (int)Math.pow(a,count);
-        checkNo(n/10,count);
+        num =num*10 + a;
+        checkNo(n/10);
         return num;
     }
     public static void main(String[] args) {
-        int n = 1633;
-        int result = checkNo(n,count(n));
-        String res = (n==result) ? "palindrome" : "not a palindrome";
+        int n = 151;
+        int result = checkNo(n);
+        String res = (n==result) ? "Palindrome" : "not a Palindrome";
         System.out.println(res);
 
     }
